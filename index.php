109e13a12cd2f00f8ae88ff38d90d50061e9c242
tobiju <t.jueschke@googlemail.com>
2015-10-11 11:59:10 +0200

    Cleanup

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
+// Composer autoloading
 $loader = require 'vendor/autoload.php';
-$loader->add('Tujo\\', __DIR__.'/library');
+$loader->add('Tujo\\', dirname('__FILE__').'/library');
 
+// Run interop-config
 $ServiceLocator = new Tujo\ServiceLocator(include dirname('__FILE__')."/config/main.php");
 $RepositoryManager= $ServiceLocator->get('repository');
 
