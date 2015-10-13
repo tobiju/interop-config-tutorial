8418ad31c66ed193f7dc7152e3d4a46d62b67f98
tobiju <t.jueschke@googlemail.com>
2015-10-13 18:25:05 +0200

    added psr-0 standard for folder structure

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 
 // Composer autoloading
 $loader = require 'vendor/autoload.php';
-$loader->add('Tujo\\', dirname('__FILE__').'/library');
+$loader->add('Tujo\\', dirname('__FILE__').'/src');
+
+$config = include dirname('__FILE__')."/config/main.php";
 
 // Run interop-config
-$ServiceLocator = new Tujo\ServiceLocator(include dirname('__FILE__')."/config/main.php");
-$RepositoryManager= $ServiceLocator->get('repository');
+$serviceLocator = new Tujo\ServiceLocator($config);
+$repositoryManager = $serviceLocator->get('repository');
 
 
 
